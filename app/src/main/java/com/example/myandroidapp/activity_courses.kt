0fe067e3ee92176ca8d988942f8959723be7b8dd
esthermanthi <manthi.estherk@gmail.com>
2020-08-14 23:40:24 +0300

    added classes

--- app/src/main/java/com/example/myandroidapp/activity_courses.kt
+++ app/src/main/java/com/example/myandroidapp/activity_courses.kt
@@ -3,9 +3,9 @@ package com.example.myandroidapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class activity_courses : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_courses)
     }
 }
--- app/src/main/java/com/example/myandroidapp/row_courses_name.kt
+++ app/src/main/java/com/example/myandroidapp/row_courses_name.kt
@@ -3,9 +3,9 @@ package com.example.myandroidapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class row_courses_name : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_row_courses_name)
     }
 }
